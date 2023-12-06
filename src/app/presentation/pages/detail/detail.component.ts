2152950052fb274140eb3c05583f59e39dfad84c
Fair Lucas <lukitaa.27@gmail.com>
2023-12-05 22:00:40 -0300

    Revert "responsive fixs"
    This reverts commit 52b5b3c4953b8f9fb4b8c9b555329e48ab8b2156.

--- src/app/presentation/pages/detail/detail.component.ts
+++ src/app/presentation/pages/detail/detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Movie } from 'src/app/data/models/movie.interface';
--- src/app/presentation/pages/login/login.component.ts
+++ src/app/presentation/pages/login/login.component.ts
@@ -8,28 +8,22 @@ import { AuthService } from 'src/app/data/services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  showPassword: boolean = false;
-  errorMessage: string = ''
   
   
   constructor(private authService: AuthService,
     private router: Router) { }
 
-    onSubmit(username: string, password: string, event: Event): void {
-      event.preventDefault();
-
-      const credentials = { userName: username, password: password };
-    
-      this.authService.login(credentials).subscribe((isLoggedIn: boolean) => {
-        if (isLoggedIn) {
-          this.router.navigate(['/home']);
-        } else {
-          this.errorMessage = 'Incorrect Validations... Try again.';
-        }
-      });
-    }
-
-  togglePasswordVisibility(): void {
-    this.showPassword = !this.showPassword;
+  onSubmit(username: string, password: string): void {
+    const credentials = { userName: username, password: password };
+  
+    this.authService.login(credentials).subscribe((isLoggedIn: boolean) => {
+      if (isLoggedIn) {
+        // Autenticación exitosa, redirige a la ruta /home
+        this.router.navigate(['/home']);
+      } else {
+        // Mostrar mensaje de error o realizar acciones para credenciales incorrectas
+        console.log('Credenciales incorrectas');
+      }
+    });
   }
 }
